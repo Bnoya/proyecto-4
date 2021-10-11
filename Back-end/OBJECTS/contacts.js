db318969e7a6152ac2011e4da719d6985771e8e3
Bnoya <benjanoya711@gmail.com>
2021-10-11 14:46:50 -0300

    CheckContact

--- Back-end/OBJECTS/contacts.js
+++ Back-end/OBJECTS/contacts.js
@@ -3,51 +3,90 @@ class Contact {
         this.sequelize = sequelize;
     }
 
-    //querryAll(){
-    //    const contact = await this.sequelize.query('SELECT id, first_name, last_name, job_position, email, company_id, region_id, country_id, city_id, contact_address, intrest FROM contact', {type: this.sequelize.QueryTypes.SELECT});
-    //    return contact;
-    //}
-    //querryByName(contacts){
-    //    const contact = await this.sequelize.query('SELECT id, first_name, last_name, job_position, email, company_id, region_id, country_id, city_id, contact_address, intrest FROM contact WHERE first_name = :first_name', { replacements: {first_name: contacts.first_name}, type: this.sequelize.QueryTypes.SELECT});
-    //    return contact;
-    //}
-    //querryByLastName(contacts){
-    //    const contact = await this.sequelize.query('SELECT id, first_name, last_name, job_position, email, company_id, region_id, country_id, city_id, contact_address, intrest FROM contact WHERE last_name = :last_name', { replacements: {last_name: contacts.last_name}, type: this.sequelize.QueryTypes.SELECT});
-    //    return contact;
-    //}
-    //querryByCompany(contacts){
-    //    const contact = await this.sequelize.query('SELECT id, first_name, last_name, job_position, email, company_id, region_id, country_id, city_id, contact_address, intrest FROM contact WHERE company_id = :company_id', { replacements: {company_id: contacts.company_id}, type: this.sequelize.QueryTypes.SELECT});
-    //    return contact;
-    //}
-    //querryByRegion(contacts){
-    //    const contact = await this.sequelize.query('SELECT id, first_name, last_name, job_position, email, company_id, region_id, country_id, city_id, contact_address, intrest FROM contact WHERE region_id = :region_id', { replacements: {region_id: contacts.region_id}, type: this.sequelize.QueryTypes.SELECT});
-    //    return contact;
-    //}
-    //querryByCountry(contacts){
-    //    const contact = await this.sequelize.query('SELECT id, first_name, last_name, job_position, email, company_id, region_id, country_id, city_id, contact_address, intrest FROM contact WHERE country_id = :country_id', { replacements: {country_id: contacts.country_id}, type: this.sequelize.QueryTypes.SELECT});
-    //    return contact;
-    //}
-    //querryByCity(contacts){
-    //    const contact = await this.sequelize.query('SELECT id, first_name, last_name, job_position, email, company_id, region_id, country_id, city_id, contact_address, intrest FROM contact WHERE city_id = :city_id', { replacements: {city_id: contacts.city_id}, type: this.sequelize.QueryTypes.SELECT});
-    //    return contact;
-    //}
-    //querryByIntrest(contacts){
-    //    const contact = await this.sequelize.query('SELECT id, first_name, last_name, job_position, email, company_id, region_id, country_id, city_id, contact_address, intrest FROM contact WHERE intrest = :intrest', { replacements: {intrest: contacts.intrest}, type: this.sequelize.QueryTypes.SELECT});
-    //    return contact;
-    //}
-//
-    //createContact(contacts){
-    //    let query;
-    //    try {
-    //        query = await this.sequelize.query("INSERT INTO contact (first_name, last_name, job_position, email, company_id, region_id, country_id, city_id, contact_address, intrest) VALUES (:first_name, :last_name, :job_position, :email, :company_id, :region_id, :country_id, :city_id, :contact_address, :intrest)", 
-    //        {
-    //            replacements: { first_name: contacts.first_name, last_name: contacts.last_name, job_position: contacts.job_position, email: contacts.email, company_id: contacts.company_id, region_id: contacts.region_id, country_id: contacts.country_id, city_id: contacts.city_id, contact_address: contacts.contact_address, intrest: contacts.intrest},
-    //            type: this.sequelize.QueryTypes.INSERT
-    //        })
-    //        return {message: 'Contacts Uploaded'}
-    //    } catch (err) {
-    //        return {message: err.errors[0].message}
-    //    }
-    //}
+    async querryAll() {
+        const contact = await this.sequelize.query('SELECT id, first_name, last_name, job_position, email, company_id, region_id, country_id, city_id, contact_address, interest FROM contact', {type: this.sequelize.QueryTypes.SELECT});
+        return contact;
+    }
+    
+    async querryById(id) {
+            const contact = await this.sequelize.query('id, first_name, last_name, job_position, email, company_id, region_id, country_id, city_id, contact_address, interest FROM contact WHERE id = :id', {
+                replacements: {id: id},
+                type: this.sequelize.QueryTypes.SELECT});
+            return contact;
+    }
+    async querryByName(id) {
+        const contact = await this.sequelize.query('id, first_name, last_name, job_position, email, company_id, region_id, country_id, city_id, contact_address, interest FROM contact WHERE first_name = :first_name', {
+            replacements: {first_name: id},
+            type: this.sequelize.QueryTypes.SELECT});
+        return contact;
+    }
+    async querryByLast(id) {
+        const contact = await this.sequelize.query('id, first_name, last_name, job_position, email, company_id, region_id, country_id, city_id, contact_address, interest FROM contact WHERE last_name = :last_name', {
+            replacements: {last_name: id},
+            type: this.sequelize.QueryTypes.SELECT});
+        return contact;
+    }
+    async querryByjob(id) {
+        const contact = await this.sequelize.query('id, first_name, last_name, job_position, email, company_id, region_id, country_id, city_id, contact_address, interest FROM contact WHERE job_position = :job_position', {
+            replacements: {job_position: id},
+            type: this.sequelize.QueryTypes.SELECT});
+        return contact;
+    }
+    async querryByEmail(id) {
+        const contact = await this.sequelize.query('id, first_name, last_name, job_position, email, company_id, region_id, country_id, city_id, contact_address, interest FROM contact WHERE email = :email', {
+            replacements: {email: id},
+            type: this.sequelize.QueryTypes.SELECT});
+        return contact;
+    }
+    async querryByCompany(id) {
+        const contact = await this.sequelize.query('id, first_name, last_name, job_position, email, company_id, region_id, country_id, city_id, contact_address, interest FROM contact WHERE company_id = :company_id', {
+            replacements: {company_id: id},
+            type: this.sequelize.QueryTypes.SELECT});
+        return contact;
+    }
+    async querryByRegion(id) {
+        const contact = await this.sequelize.query('id, first_name, last_name, job_position, email, company_id, region_id, country_id, city_id, contact_address, interest FROM contact WHERE region_id = :region_id', {
+            replacements: {region_id: id},
+            type: this.sequelize.QueryTypes.SELECT});
+        return contact;
+    }
+    async querryByCountry(id) {
+        const contact = await this.sequelize.query('id, first_name, last_name, job_position, email, company_id, region_id, country_id, city_id, contact_address, interest FROM contact WHERE country_id = :country_id', {
+            replacements: {country_id: id},
+            type: this.sequelize.QueryTypes.SELECT});
+        return contact;
+    }
+    async querryByCity(id) {
+        const contact = await this.sequelize.query('id, first_name, last_name, job_position, email, company_id, region_id, country_id, city_id, contact_address, interest FROM contact WHERE city_id = :city_id', {
+            replacements: {city_id: id},
+            type: this.sequelize.QueryTypes.SELECT});
+        return contact;
+    }
+    async querryByAddress(id) {
+        const contact = await this.sequelize.query('id, first_name, last_name, job_position, email, company_id, region_id, country_id, city_id, contact_address, interest FROM contact WHERE contact_address = :contact_address', {
+            replacements: {contact_address: id},
+            type: this.sequelize.QueryTypes.SELECT});
+        return contact;
+    }
+    async querryByInterest(id) {
+        const contact = await this.sequelize.query('id, first_name, last_name, job_position, email, company_id, region_id, country_id, city_id, contact_address, interest FROM contact WHERE interest = :interest', {
+            replacements: {interest: id},
+            type: this.sequelize.QueryTypes.SELECT});
+        return contact;
+    }
+
+    async createContact(contact) {
+        let query;
+        try {
+            query = await this.sequelize.query("INSERT INTO contact (first_name, last_name, job_position, email, company_id, region_id, country_id, city_id, contact_address, interest) VALUES (:first_name, :last_name, :job_position, :email, :company_id, :region_id, :country_id, :city_id, :contact_address, :interest)", 
+            {
+                replacements: { first_name: contact.first_name, last_name: contact.last_name, job_position: contact.job_position, email: contact.email, company_id: contact.company_id, region_id: contact.region_id, country_id: contact.country_id, city_id: contact.city_id, contact_address: contact.contact_address, interest: contact.interest},
+                type: this.sequelize.QueryTypes.INSERT
+            })
+            return {message: 'Contact Uploaded'}
+        } catch (err) {
+            return {message: err.errors[0].message}
+        }
+    }
 }
-module.exports = Contact;
+module.exports = Contact
